3818ec724f9ec1bb760417319d7839fb8e1a7d41
qeery8 <daniil.chernykh133@gmail.com>
2024-11-01 14:18:37 +0300

    Update wb1.go

--- wb1.go
+++ wb1.go
@@ -2,28 +2,39 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-func square(num int, wg *sync.WaitGroup, ch chan int) {
-	defer wg.Done()
-	ch <- num * num
+type Human struct {
+	Name string
+	Age  int
+}
+
+func (h *Human) Speak() {
+	fmt.Printf("Hello, my name is %s and I am %d years old.\n", h.Name, h.Age)
+}
+
+func (h *Human) Walk() {
+	fmt.Println("I'm walking...")
+}
+
+type Action struct {
+	Human
+	ActionType string
+}
+
+func (a *Action) DoAction() {
+	fmt.Printf("%s is performing action: %s\n", a.Name, a.ActionType)
 }
 
 func main() {
-	num := []int{2, 4, 6, 8, 10}
-	ch := make(chan int, len(num))
-	var wg sync.WaitGroup
 
-	for _, n := range num {
-		wg.Add(1)
-		go square(n, &wg, ch)
-	}
-	wg.Wait()
-	close(ch)
-
-	for result := range ch {
-		fmt.Println(result)
+	person := Action{
+		Human:      Human{Name: "Alice", Age: 30},
+		ActionType: "Running",
 	}
 
+	person.Speak()
+	person.Walk()
+
+	person.DoAction()
 }
